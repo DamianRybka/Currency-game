9e1db7448a8caced35b6d03f1a5ef0326b8dde1a
DamianRybka <damian.rybka@op.pl>
2019-08-17 10:25:21 +0200

    Initial commit

---
+++ src/main/java/com/rybka/currencygame/controller/CurrencyClient.java
@@ -0,0 +1,19 @@
+package com.rybka.currencygame.controller;
+
+import com.rybka.currencygame.model.CurrencyModel;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class CurrencyClient {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void getCurrencyRates() {
+
+        RestTemplate restTemplate = new RestTemplate(); // łączenie się z API - pozwala na to
+        CurrencyModel forObject = restTemplate.getForObject("https://api.exchangeratesapi.io/latest?base=PLN", CurrencyModel.class);
+        System.out.println(forObject);
+    }
+}
